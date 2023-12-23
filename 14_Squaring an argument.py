684758c0355e1263998eaac35d206b64d9ba6ac9
Voxold <bilalajaha@gmail.com>
2023-12-24 00:26:19 +0100

    Python P.S

---
+++ 14_Squaring an argument.py
@@ -0,0 +1,18 @@
+# Write a function that takes an argument and returns the square of it.
+
+# Method 1 :
+def square(n):
+    return n*n
+
+# Method 2 :
+def square(n):
+    return n ** 2
+
+# Method 2 :
+def square(n):
+    return pow(n,2)
+
+print(square(2))
+print(square(3))
+print(square(5))
+print(square(10))
