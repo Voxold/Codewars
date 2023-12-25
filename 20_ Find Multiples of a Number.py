29f96dc97e789c9ac2fb105e4e3dc79c45d9df3c
Voxold <bilalajaha@gmail.com>
2023-12-25 21:54:13 +0100

    P.S

---
+++ 20_ Find Multiples of a Number.py
@@ -0,0 +1,13 @@
+#  Find Multiples of a Number
+
+def find_multiples(integer, limit):
+    result = []
+    x = 1 
+    while x <= int(limit /integer):
+        result.append(integer * x)
+        x += 1
+    return result
+
+print(find_multiples(2,6))
+print(find_multiples(2,7))
+print(find_multiples(5,21))
