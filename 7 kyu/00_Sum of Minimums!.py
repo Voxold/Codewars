24f7b263bf3379cc409fa7ad540456830d3c4a4a
Voxold <bilalajaha@gmail.com>
2024-01-14 18:02:26 +0100

    for loop

--- 7 kyu/00_Sum of Minimums!.py
+++ 7 kyu/00_Sum of Minimums!.py
@@ -2,8 +2,11 @@
 # your task is to find the sum of the minimum values in each row.
 
 def sum_of_minimums(numbers):
-    return min(numbers)
+    list = []
+    for i in range(len(numbers)):
+        list.append(min(numbers[i]))
+    return sum(list)
     
-print(sum_of_minimums([ 1, 2, 3, 4, 5 ]))       #  minimum value of row is 1
-print(sum_of_minimums([ 5, 6, 7, 8, 9 ]))       #  minimum value of row is 5
-print(sum_of_minimums([ 20, 21, 34, 56, 100 ])) #  minimum value of row is 20
+print(sum_of_minimums([ [ 1, 2, 3, 4, 5 ]               #  minimum value of row is 1
+                        , [ 5, 6, 7, 8, 9 ]             #  minimum value of row is 5
+                        , [ 20, 21, 34, 56, 100 ]]))    #  minimum value of row is 20
