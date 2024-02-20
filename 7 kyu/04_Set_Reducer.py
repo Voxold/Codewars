9bf4bbf2769b95e8464c2350229dccf1cc59be45
Voxold <bilalajaha@gmail.com>
2024-02-20 22:41:36 +0100

    task 4

---
+++ 7 kyu/04_Set_Reducer.py
@@ -0,0 +1,20 @@
+# These arrays are too long! Let's reduce them!
+
+def set_reducer(inp):
+    while len(inp) > 1:
+        list = []
+        i = 0
+        while i < len(inp):
+            count = 1
+            while i + count < len(inp) and inp [i + count] == inp[i]:
+                count+=1
+                if count > 1:
+                    list.append(count)
+                else:
+                    list.append(1)
+            i+=1
+        inp = list
+        return inp[0]
+
+print(set_reducer([0, 4, 6, 8, 8, 8, 5, 5, 7]))
+print(set_reducer([8, 1, 6, 1, 2, 7, 7, 7, 7, 6, 5, 3, 2, 1, 8]))
