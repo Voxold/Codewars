3df716ed6c721e3b3eebf406c60ee4faa0ecb060
Voxold <bilalajaha@gmail.com>
2024-02-20 22:54:47 +0100

    task 3

---
+++ 7 kyu/05_Product of Largest Pair.py
@@ -0,0 +1,12 @@
+# Rick wants a faster way to get the product of the largest pair in an array.
+
+def max_product(a):
+	list = sorted(a)
+	
+
+
+
+
+max_product([2, 1, 5, 0, 4, 3])              # => 20
+max_product([7, 8, 9])                       # => 72
+max_product([33, 231, 454, 11, 9, 99, 57])
